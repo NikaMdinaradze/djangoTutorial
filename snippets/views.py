161e59c3ea2868ef47493f2fbd6e960c2e960944
NikaMdinaradze <mdinaradzenika04@gmail.com>
2023-12-12 19:08:21 +0400

    auth

--- snippets/views.py
+++ snippets/views.py
@@ -1,52 +1,24 @@
 from .models import Snippet
-from .serializers import SnippetSerializer
-from rest_framework import status
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
+from .serializers import SnippetSerializer, UserSerializer
+from rest_framework import generics
+from django.contrib.auth.models import User
 
 
-# Create your views here.
-@api_view(['GET', 'POST'])
-def snippet_list(request, format=None):
-    """
-    List all code snippets, or create a new snippet.
-    """
-    if request.method == "GET":
-        snippets = Snippet.objects.all()
-        serialized = SnippetSerializer(snippets, many=True)
-        return Response(serialized.data, status=status.HTTP_200_OK)
-
-    elif request.method == "POST":
-        serialized = SnippetSerializer(request.data)
-        if serialized.is_valid():
-            serialized.save()
-            Response(serialized.data, status=status.HTTP_201_CREATED)
-        return Response(serialized.errors, status=status.HTTP_400_BAD_REQUEST)
-
-@api_view(['GET','DELETE','PUT'])
-def snippet_detail(request, pk, format=None):
-    """
-    Retrieve, update or delete a code snippet.
-    """
-    try:
-        snippet = Snippet.objects.get(pk=pk)
-    except Snippet.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-
-    if request.method == 'GET':
-        serialized = SnippetSerializer(snippet)
-        return Response(serialized.data, status=status.HTTP_200_OK)
-
-    elif request.method == 'PUT':
-        serialized = SnippetSerializer(snippet, request.data)
-        if serialized.is_valid():
-            serialized.save()
-            return Response(serialized.data, status=status.HTTP_201_CREATED)
-        return Response(serialized.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    elif request.method == 'DELETE':
-        snippet.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
 
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
+
+class SnippetList(generics.ListCreateAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+
+
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
